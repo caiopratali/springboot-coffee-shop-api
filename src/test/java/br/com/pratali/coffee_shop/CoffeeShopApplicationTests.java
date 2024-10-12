5d99f8689bbf3056dfbb63f000bf030910da77ad
Caio Pratali <caio@pratali.com.br>
2024-10-12 18:28:03 -0300

    first commit

---
+++ src/test/java/br/com/pratali/coffee_shop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pratali.coffee_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
